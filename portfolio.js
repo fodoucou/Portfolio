2a745efe4b908b8d4ad400c736dc43895b4055da
fodoucou <doucoure-f@hotmail.fr>
2024-08-07 14:26:16 +0200

    ajout du premier jet du portfolio

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+
+
+//function to add interactivity on the website
+
+let bod= document.body.style; //recuperer l attribut style du body
+//let bouger = document.getElementById("unun").onclick(bod.backgroundColor="red");
+
+//changer la couleur du background
+function bouger(){
+    bod.backgroundColor="red";
+}
